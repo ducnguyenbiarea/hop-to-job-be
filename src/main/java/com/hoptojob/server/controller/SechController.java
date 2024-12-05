864aeea7b404d99d9670d58adca400e780c77175
ducnguyenbiarea <128296098+ducnguyenbiarea@users.noreply.github.com>
2024-12-05 23:07:52 +0700

    Add files via upload

---
+++ src/main/java/com/hoptojob/server/controller/SechController.java
@@ -0,0 +1,20 @@
+package com.hoptojob.server.controller;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class SechController implements ErrorController {
+    @RequestMapping("/error")
+    public String handleError() {
+        // Quản lý lỗi và trả về trang tùy chỉnh
+        return "error"; // Trang lỗi có thể là một file error.html trong thư mục templates
+    }
+
+    public String getErrorPath() {
+        return "/error";
+    }
+}
+
+
